3f84e6c4969ae867f5f6c7010f098f94b34a3586
Reka <rekapap28@gmail.com>
2018-10-16 13:13:02 +0100

    refactoring

--- Leetcode/13.rb
+++ Leetcode/13.rb
@@ -10,20 +10,18 @@ def roman_to_int(s)
     D: 500,
     M: 1000
   }
-  specials = {
+  special_rules = {
     I: %i[V X],
     X: %i[L C],
     C: %i[D M]
   }
   sum = 0
-  special_array = specials.keys
+  special_keys = special_rules.keys
   symbols = s.chars.map(&:to_sym)
   i = 0
-  symbols.each do |c|
-    ch = c.to_sym
-    next_ch = s[i + 1]
-    next_ch = next_ch.to_sym unless next_ch.nil?
-    if special_array.include?(ch) && specials[ch].include?(next_ch)
+  symbols.each do |ch|
+    next_ch = symbols[i + 1]
+    if special_keys.include?(ch) && special_rules[ch].include?(next_ch)
       sum -= hash[ch]
     else
       sum += hash[ch]
@@ -33,4 +31,5 @@ def roman_to_int(s)
   sum
 end
 
-puts roman_to_int('MCMXCIV')
+# TESTS
+# puts roman_to_int('MCMXCIV')
